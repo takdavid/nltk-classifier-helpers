511e8611f774a14b4c7d0cbf06d002e8c1c25187
Takacs, David <takdavid@gmail.com>
2013-11-26 21:08:05 +0100

    Adding stuff for use with virtualenv.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nltk-classifier-helpers',
+    version='0.0.1',
+    author_email='takdavid@gmail.com',
+    namespace_packages=['nltk_classifier_helpers'],
+    packages=find_packages(),
+    url='https://github.com/takdavid/nltk-classifier-helpers.git',
+    description='Helper tools for the classifiers in NLTK.',
+    long_description='',
+    )
+
