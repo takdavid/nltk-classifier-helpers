620a735b46cff1410df02b55ba434477a7496ed3
takdavid <takdavid@gmail.com>
2020-08-11 12:04:46 +0200

    Upgrade dependency for Dependabot. And it is not supposed to be a namespace package.

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='nltk-classifier-helpers',
-    version='0.0.1',
+    version='0.0.2',
     author_email='takdavid@gmail.com',
-    namespace_packages=['nltk_classifier_helpers'],
     packages=find_packages(),
     url='https://github.com/takdavid/nltk-classifier-helpers.git',
     description='Helper tools for the classifiers in NLTK.',
